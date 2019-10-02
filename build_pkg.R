07a96313934a89770e35f4c1ad5c3f078b956bc2
Walmes Zeviani <walmes@ufpr.br>
2019-10-02 16:01:02 -0300

    Adds pkgdown support.

--- build_pkg.R
+++ build_pkg.R
@@ -14,11 +14,16 @@ devtools::document()
 
 # Faz verificação de conformidade do pacote.
 # devtools::check(env_vars = c("_R_CHECK_LICENSE_" = FALSE))
-devtools::check()
+devtools::check(args = "--no-build-vignettes",
+                build_args = "--no-build-vignettes",
+                env_vars = c("_R_CHECK_LICENSE_" = FALSE))
 
 # Gera o tar.gz do pacote.
 devtools::build(vignettes = FALSE,
                 manual = FALSE)
+
+# Gera as vinhetas.
+# build_vignettes(dependencies = FALSE)
 
 # Instala o pacote.
 devtools::install(reload = TRUE,
@@ -28,6 +33,14 @@ devtools::install(reload = TRUE,
 
 devtools::session_info()
 sessionInfo()
+
+#-----------------------------------------------------------------------
+# Gera a página em pkgdown do pacote.
+
+library(pkgdown)
+
+# Constrói os elementos da página do pacote.
+pkgdown::build_site()      # Todo o site que inclui tudo acima.
 
 #-----------------------------------------------------------------------
 # Inspecina o pacote.
@@ -51,6 +64,7 @@ ls()
 files <- dir(path = system.file("images", package = "ClickMetrics"),
              pattern = "\\.png$",
              full.names = TRUE)
+files
 
 diameter_measurer(
     files = files,
